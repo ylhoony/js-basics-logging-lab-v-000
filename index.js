727acd0ce2fdf10ec02cf4e3b6ca3cc9e84186a3
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 16:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,5 @@ const driver = 'bob';
 
 console.log('this code was called');
 
-const calledWithExactly = console.log(driver);
-
 const calledWithExactly = console.log(driver, 'is the driver variable name');
 
-// function calledWithExactly()
-
-// 
-//   it('invokes the console.log with the `driver` variable as the first argument and `is the driver variable name` as the second argument', function () {
-//     expect(spy.calledWithExactly('bob', 'is the driver variable name')).to.be.true;
-//   });
-// });
-// 
