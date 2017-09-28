a7549d3e069194c9958eabf7750549eaa3092edf
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 16:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const driver = 'bob';
 
 console.log('this code was called');
 
-let calledWithExactly = console.log(driver);
+// const calledWithExactly = console.log(driver);
 
 // const calledWithExactly = console.log(driver, 'is the driver variable name');
 
+function calledWithExactly(driver, sentence) {
+  console.log(driver, sentence);
+}
