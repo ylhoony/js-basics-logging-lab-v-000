3074f2e97efb88e67c68742365157f2a6cb049e1
Hoon Lee <yl.hoony@gmail.com>
2017-09-28 16:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,8 @@ const driver = 'bob';
 
 console.log('this code was called');
 
+const calledWithExactly = console.log(driver);
 
-// 
-// describe('console.log()', function () {
-//   const spy = sinon.spy(console, 'log');
-// 
-//   after(function () {
-//     spy.restore();
-//   });
-// 
-//   it('invokes console.log() with the string "this code was called"', function () {
-//     expect(spy.calledWithExactly('this code was called')).to.be.true;
-//   });
 // 
 //   it('invokes console.log() with the `driver` variable', function () {
 //     expect(spy.calledWithExactly('bob')).to.be.true;
